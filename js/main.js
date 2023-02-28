8cb73db958c814b309cd79a821e6d614a19694a7
jobarv <jbarrera@vabaja.com.mx>
2023-02-27 19:51:04 -0600

    enlaces modal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-const toggleModal = ((e) => {
-  const modal = document.getElementById("modalContainer");
+const toggleModal1 = ((e) => {
+  const modal = document.getElementById("modalContainer--SE");
+  if (modal.style.display === "none") {
+    modal.style.display = "block";
+    trapped = trapFocus(modal);
+  } else {
+    modal.style.display = "none";
+    trapped.onClose();
+  } 
+})
+const toggleModal2 = ((e) => {
+  const modal = document.getElementById("modalContainer--cobranzas");
+  if (modal.style.display === "none") {
+    modal.style.display = "block";
+    trapped = trapFocus(modal);
+  } else {
+    modal.style.display = "none";
+    trapped.onClose();
+  } 
+})
+const toggleModal3 = ((e) => {
+  const modal = document.getElementById("modalContainer--ventas");
   if (modal.style.display === "none") {
     modal.style.display = "block";
     trapped = trapFocus(modal);
