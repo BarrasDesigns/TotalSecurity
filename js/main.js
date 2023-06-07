3e60adc54900f3023adec319414283c3eb072dd2
jobarv <jbarrera@motiontech.com.mx>
2023-06-06 18:01:11 -0600

    Se agrega video final con boton

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
+// REPRODUCE VIDEO 
+function getFullscreenElement() {
+  return document.fullscreenElement
+    || document.webkitFullscreenElement
+    || document.mozFullscreenElement
+    || document.msFullscreenElement
+}
+
+function iniciar() {
+  let boton = document.getElementById('btnIntro');
+  boton.addEventListener('click', presionar, false);
+}
+function presionar() {
+  let video = document.getElementById('intro');
+  video.play();
+}
+window.addEventListener('load', iniciar, false);
+
+function toggleFullscreen() {
+  if (getFullscreenElement()) {
+    document.exitFullscreen();
+  } else {
+    document.getElementById("intro").requestFullscreen().catch(console.log);
+    document.getElementById("intro").requestFullscreen().catch((e) => {
+      document.getElementById("intro").style.display = "block";
+      HTMLMediaElement.play();
+    });
+  }
+}
+
+function addEvent(element, evnt, funct) {
+  if (element.attachEvent)
+    return element.attachEvent('on' + evnt, funct);
+  else
+    return element.addEventListener(evnt, funct, false);
+}
+
+addEvent(
+  document.getElementById('fullscreen'),
+  'click',
+  function () { toggleFullscreen(); }
+);
+
+document.getElementById("intro").style.display = "none";
+
 const toggleModal1 = ((e) => {
   const modal = document.getElementById("modalContainer--ventas");
   if (modal.style.display === "none") {
@@ -6,7 +51,7 @@ const toggleModal1 = ((e) => {
   } else {
     modal.style.display = "none";
     trapped.onClose();
-  } 
+  }
 })
 const toggleModal2 = ((e) => {
   const modal = document.getElementById("modalContainer--hikStore");
@@ -16,7 +61,7 @@ const toggleModal2 = ((e) => {
   } else {
     modal.style.display = "none";
     trapped.onClose();
-  } 
+  }
 })
 const toggleModal3 = ((e) => {
   const modal = document.getElementById("modalContainer--cobranza");
@@ -26,7 +71,7 @@ const toggleModal3 = ((e) => {
   } else {
     modal.style.display = "none";
     trapped.onClose();
-  } 
+  }
 })
 const toggleModal4 = ((e) => {
   const modal = document.getElementById("modalContainer--atencionAclientes");
@@ -36,7 +81,7 @@ const toggleModal4 = ((e) => {
   } else {
     modal.style.display = "none";
     trapped.onClose();
-  } 
+  }
 })
 const toggleModal5 = ((e) => {
   const modal = document.getElementById("modalContainer--soporteTecnico");
@@ -46,5 +91,5 @@ const toggleModal5 = ((e) => {
   } else {
     modal.style.display = "none";
     trapped.onClose();
-  } 
+  }
 })
